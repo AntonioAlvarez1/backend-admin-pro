d7516fcdd9529a91e2bda3d08e86e15ebf3e8711
AntonioAlvarez1 <antonio.alvarez98a@gmail.com>
2023-09-14 03:38:44 -0600

    feat: add JWT token in login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from 'src/users/users.module';
 
@@ -6,7 +8,21 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '1m',
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
