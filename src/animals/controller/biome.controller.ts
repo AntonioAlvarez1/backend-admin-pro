992b0d947fb900b889641582bf03d8ca65d8b73a
AntonioAlvarez1 <antonio.alvarez98a@gmail.com>
2023-10-19 10:57:54 -0600

    feat: add auth in controllers

--- src/animals/controller/biome.controller.ts
+++ src/animals/controller/biome.controller.ts
@@ -4,8 +4,10 @@ import { BiomeService } from '../services/biome.service';
 import { CreateBiomeDto } from '../dto';
 import { MyResponse } from 'src/core';
 import { Biome } from '../entities';
+import { Auth } from 'src/auth';
 
 @Controller('biome')
+@Auth()
 export class BiomeController {
   constructor(private readonly biomeService: BiomeService) {}
 
--- src/animals/controller/species.controller.ts
+++ src/animals/controller/species.controller.ts
@@ -10,8 +10,10 @@ import { SpeciesService } from '../services';
 import { CreateSpeciesDto } from '../dto/create-species.dto';
 import { MyResponse } from 'src/core';
 import { Species } from '../entities';
+import { Auth } from 'src/auth';
 
 @Controller('species')
+@Auth()
 export class SpeciesController {
   constructor(private readonly speciesService: SpeciesService) {}
 
