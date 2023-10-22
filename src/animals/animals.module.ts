3c510631989e8cf28db86bd479db1b5243d0d869
AntonioAlvarez1 <antonio.alvarez98a@gmail.com>
2023-10-21 18:13:00 -0600

    fix: Fix error PassportModule

--- src/animals/animals.module.ts
+++ src/animals/animals.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from 'src/auth';
 import { AnimalsService, SpeciesService } from './services';
 import { AnimalsController, SpeciesController } from './controller';
 import { Animal, Species, Biome, Diet } from './entities';
@@ -10,7 +11,10 @@ import { DietService } from './services/diet.service';
 import { DietController } from './controller/diet.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Animal, Species, Biome, Diet])],
+  imports: [
+    TypeOrmModule.forFeature([Animal, Species, Biome, Diet]),
+    AuthModule,
+  ],
   controllers: [
     AnimalsController,
     SpeciesController,
